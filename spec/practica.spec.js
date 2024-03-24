7000798458c5019420f3ab3d00224fad09f9fe04
Sharonesaa <oriannaesaa07@gmail.com>
2024-03-24 10:29:00 -0300

    Se separa en 2 archivos ya que no me dejaba ejecutar las pruebas con el DOM (DOM, ARCHIVO A TESTEAR) y se realizan pruebas jasmine.

---
+++ spec/practica.spec.js
@@ -0,0 +1,23 @@
+const{Activity, Repository} = require("../scripts/index");
+
+describe("Activity y Repository son clases", function () {
+    it("El constructor de Activity debe recibir parámetros", function () {
+        expect(Activity).toBeDefined(); // Verifica que Activity esté definida
+        expect(Activity.prototype.constructor.length).toBeGreaterThan(3);
+    });
+
+    it("Repository debe ser una clase", function () {
+        expect(Repository).toBeDefined(); // Verifica que Repository esté definida
+        expect(typeof Repository.prototype.constructor).toBe("function");
+    });
+
+    it("Debe existir un metodo getAllActivities()", function () {
+      const repository = new Repository();
+      expect(repository.getAllActivities).toBeDefined();
+    });
+
+  it("El metodo getAllActivities() debe retornar un array", function () {
+    const repository = new Repository();
+    expect(Array.isArray(repository.getAllActivities())).toBeTrue();
+  });
+});
